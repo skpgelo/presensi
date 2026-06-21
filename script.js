eb0f54ac61da9abf1cf3c0ca3198936aa23f7efd
skpgelo <skpgelo137@gmail.com>
2026-06-21 16:16:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzN_KVjz6e9x-INX26wSCAX2l3t4ggb9FEjlmlCscAydCCEPk6K432iI9V_9RITs56PNA/exec'
+const sheetName = 'Sheet1'
+const scriptProp = PropertiesService.getScriptProperties()
 
-const form = document.forms['contact-form']
+function intialSetup () {
+  const activeSpreadsheet = SpreadsheetApp.getActiveSpreadsheet()
+  scriptProp.setProperty('key', activeSpreadsheet.getId())
+}
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
+function doPost (e) {
+  const lock = LockService.getScriptLock()
+  lock.tryLock(10000)
 
+  try {
+    const doc = SpreadsheetApp.openById(scriptProp.getProperty('key'))
+    const sheet = doc.getSheetByName(sheetName)
+
+    const headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0]
+    const nextRow = sheet.getLastRow() + 1
+
+    const newRow = headers.map(function(header) {
+      return header === 'Date' ? new Date() : e.parameter[header]
+    })
+
+    sheet.getRange(nextRow, 1, 1, newRow.length).setValues([newRow])
+
+    return ContentService
+      .createTextOutput(JSON.stringify({ 'result': 'success', 'row': nextRow }))
+      .setMimeType(ContentService.MimeType.JSON)
+  }
+
+  catch (e) {
+    return ContentService
+      .createTextOutput(JSON.stringify({ 'result': 'error', 'error': e }))
+      .setMimeType(ContentService.MimeType.JSON)
+  }
+
+  finally {
+    lock.releaseLock()
+  }
+}
